b2cc4bd6deed856dd3c91a7bc95c56f81e456c1a
tatyana kuleva <tatianaarsentievna@gmail.com>
2025-04-28 15:57:33 +0300

    func for second part first exc added

--- src/views.py
+++ src/views.py
@@ -3,7 +3,9 @@ import json
 from src.func_get_data import read_excel_file, get_users_settings
 from src.utils import (get_start_of_period, greeting_user, filtr_transction_by_date, filtr_operation_with_cashback,
                        agregate_transaction_card, get_top_transaction, get_currency_rate, get_stocks_rate,
-                       filtr_transction_by_period, sort_only_expenses, get_sum_by_column, group_by_category_for_main)
+                       filtr_transction_by_period, filtr_only_expenses, get_sum_by_column, group_sort_by_category,
+                       extract_main_category, top_seven_category_main,extract_transfer_and_cash_category,
+                       other_category_main_sum)
 
 
 def main(current_date: str):
@@ -55,17 +57,27 @@ def event(date:str, data_range='M'):
 
     filtr_transaction = filtr_transction_by_period(df, start, date)
 
-    only_exp = sort_only_expenses(filtr_transaction)
+    only_exp = filtr_only_expenses(filtr_transaction)
 
     # result = get_sum_by_column(only_exp)
 
-    result = group_by_category_for_main(only_exp)
+    result_1 = group_sort_by_category(only_exp)
 
-    # result = extract_data.to_dict('records')
+    result_2 = extract_main_category(result_1, 'Переводы', 'Наличные')
+
+    result = top_seven_category_main(result_2)
+
+    result_4 = other_category_main_sum(result_2)
+
+    result_5 = get_sum_by_column(result_4, 'sum_pay')
+
+    result_3= extract_transfer_and_cash_category(result_1, 'Переводы', 'Наличные')
+
+    # result = result.to_dict('records')
 
     # json_res = json.dumps(result, indent=4, ensure_ascii=False)
 
-    return result
+    return result_5
 
 if __name__ == '__main__':
-    print(event('2019-07-17 15:05:27', 'ALL'))
+    print(event('2019-07-17 15:05:27', 'Y'))
